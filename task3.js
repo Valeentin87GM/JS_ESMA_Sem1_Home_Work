2673520e0393e2593637464a46ad501009773bdc
Valeentin87 <valeentin87@list.ru>
2024-07-06 11:50:02 +0300

    успешное решение задачи №3

--- task3.js
+++ task3.js
@@ -1,15 +1,18 @@
-// напишите рекурсивную функцию FindElementWithClass, которая принимает корневой элемент дерева DOM
+// напишите рекурсивную функцию FindElementWithClass, которая принимает элемент дерева DOM
 // и название класса в качестве аргументов и возвращает первый найденный элемент с указанным классом в этом дереве
 
+var element = document.querySelector('.class6');
 
+function findElementWithClass(sourceElement, className){
+    var element = sourceElement; //selected DOM node
 
-function findElementWithClass(sourceElement, className) {
-    const element = sourceElement;
-    while(element !== document.body) {
-        if (element.classList.contains(className)) {
+    while(element !== document.body){
+        if(element.classList.contains(className)){
             break;
         }
         element = element.parentNode;
     }
     return element;
 }
+
+console.log(findElementWithClass(element, 'class3'));
